f7decbffe1dc0b42ff557b7677ef76e66f931d6e
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2025-10-31 00:02:40 +0300

    Update File.c

--- PYAS_Protection/File.c
+++ PYAS_Protection/File.c
@@ -169,8 +169,8 @@ OB_PREOP_CALLBACK_STATUS PreCallBack(
     static const PCWSTR protectedPatterns[] = {
         L"\\HydraDragonAntivirus\\HydraDragonAntivirusLauncher.exe",
         L"\\HydraDragonAntivirus\\HydraDragonAntivirusLauncher.dll", // WARNING: Some antivirus programs (like Malwarebytes or Ikarus) may be unable to remove HydraDragon Antivirus and might mistakenly flag your system as infected because of it.
-        L"\\Owlyshield Service\\owlyshield_ransom.exe",
-        L"\\Owlyshield Service\\tensorflowlite_c.dll",
+        L"Service\\owlyshield_ransom.exe",
+        L"Service\\tensorflowlite_c.dll",
         L"\\OwlyshieldRansomFilter\\OwlyshieldRansomFilter.sys",
         L"\\drivers\\MBRFilter.sys",
         L"\\sanctum\\app.exe",
